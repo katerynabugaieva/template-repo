a7d7536049cc3af26b250f01ca133fde031922b2
Kateryna Bugaieva <sonnensmile@gmail.com>
2020-12-25 22:34:27 +0100

    :wrench: update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
-import { css } from "@emotion/core"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
-import { rhythm } from "../utils/typography"
 
 const Divider = styled.div`
    width: 100%;
@@ -12,70 +10,25 @@ const Divider = styled.div`
    margin: 10px 0px;
   }
 `
-const Item = styled.h5`
-    margin-bottom: ${rhythm(1 / 4)};
-  }
-`
+
 export default function Home({ data }) {
+  console.log(data)
   return (
     <Layout>
       <div>
-        <h3>Ship of Theseus</h3>
-        <Link
-          to="/fromWiki"
-          css={css`
-            color: inherit;
-            text-decoration: none;
-          `}
-        >
-          <Item>Something that you can read in wikipedia</Item>
-        </Link>
-
+        <h3>{data.site.siteMetadata.title}</h3>
         <Divider />
-
-        <Link
-          to="/description"
-          css={css`
-            color: inherit;
-            text-decoration: none;
-          `}
-        >
-          <Item>Description</Item>
-        </Link>
-        <Divider />
-
-        <Link
-          to="/development"
-          css={css`
-            color: inherit;
-            text-decoration: none;
-          `}
-        >
-          <Item>Development</Item>
-        </Link>
-        <Divider />
-
-        <Link
-          to="/notFromWiki"
-          css={css`
-            color: inherit;
-            text-decoration: none;
-          `}
-        >
-          <Item>Something that you cannot read in wikipedia</Item>
-        </Link>
-
-        <Divider />
-        <Link
-          to="/booksImagies"
-          css={css`
-            color: inherit;
-            text-decoration: none;
-          `}
-        >
-          <Item>Images of the book</Item>
-        </Link>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
